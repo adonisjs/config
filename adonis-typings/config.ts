8bbc111f1fd79f93c7d16f3ca2698c648610b7d4
Harminder virk <virk.officials@gmail.com>
2019-12-21 12:56:23 +0530

    style: update docblock

--- adonis-typings/config.ts
+++ adonis-typings/config.ts
@@ -9,9 +9,24 @@
 
 declare module '@ioc:Adonis/Core/Config' {
   export interface ConfigContract {
+    /**
+     * Get value for a key from the config store
+     */
     get (key: string, defaultValue?: any): any
+
+    /**
+     * Merge values for a key with the default values
+     */
     merge (key: string, defaultValues: object, customizer?: Function): any
+
+    /**
+     * Set value for a key
+     */
     set (key: string, value: any): void
+
+    /**
+     * Define defaults that will be merged with the user defined config.
+     */
     defaults (key: string, value: any): void
   }
 
--- providers/ConfigProvider.ts
+++ providers/ConfigProvider.ts
@@ -1,7 +1,3 @@
-/**
- * @module @adonisjs/config
- */
-
 /*
 * @adonisjs/config
 *
