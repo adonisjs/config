f3509aa1946d7bb58dc0c67582274e17793279d1
Harminder virk <virk.officials@gmail.com>
2020-07-05 12:46:39 +0530

    feat: add prettier

--- providers/ConfigProvider.ts
+++ providers/ConfigProvider.ts
@@ -1,11 +1,11 @@
 /*
-* @adonisjs/config
-*
-* (c) Harminder Virk <virk@adonisjs.com>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ * @adonisjs/config
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 import { requireAll } from '@poppinss/utils'
 import { IocContract } from '@adonisjs/fold'
@@ -13,12 +13,12 @@ import { IocContract } from '@adonisjs/fold'
 import { Config } from '../src/Config'
 
 export default class ConfigProvider {
-  constructor (protected container: IocContract) {}
+	constructor(protected container: IocContract) {}
 
-  public register () {
-    this.container.singleton('Adonis/Core/Config', () => {
-      const app = this.container.use('Adonis/Core/Application')
-      return new Config(requireAll(app.configPath()))
-    })
-  }
+	public register() {
+		this.container.singleton('Adonis/Core/Config', () => {
+			const app = this.container.use('Adonis/Core/Application')
+			return new Config(requireAll(app.configPath()))
+		})
+	}
 }
