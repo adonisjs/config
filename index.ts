e811209985804e35eedd815c37e5d7c39140ab22
Harminder Virk <virk.officials@gmail.com>
2022-10-20 17:46:20 +0530

    refactor: rewrite in ESM

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 /*
  * @adonisjs/config
  *
- * (c) Harminder Virk <virk@adonisjs.com>
+ * (c) AdonisJS
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-export { Config } from './src/Config'
+export { Config } from './src/config.js'
--- src/Config.ts
+++ src/Config.ts
@@ -1,22 +1,19 @@
 /*
  * @adonisjs/config
  *
- * (c) Harminder Virk <virk@adonisjs.com>
+ * (c) AdonisJS
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-/// <reference path="../adonis-typings/config.ts" />
-
-import { lodash } from '@poppinss/utils'
-import { ConfigContract } from '@ioc:Adonis/Core/Config'
+import lodash from '@poppinss/utils/lodash'
 
 /**
  * Config module eases the process of using configuration inside your AdonisJs
  * applications.
  *
- * The config files are stored inside a seperate directory, which are loaded and cached
+ * The config files are stored inside a dedicated directory, which are loaded and cached
  * on application boot. Later you can access the values using the `dot` syntax.
  *
  * ## Access values
@@ -38,61 +35,41 @@ import { ConfigContract } from '@ioc:Adonis/Core/Config'
  * **NOTE:**
  * The `get` method doesn't raise runtime exceptions when top level objects are missing.
  */
-export class Config implements ConfigContract {
-  constructor(private config = {}) {}
+export class Config {
+  #config: Record<any, any>
 
-  /**
-   * Returns complete config
-   */
-  public all() {
-    return this.config
+  constructor(config: Record<any, any> = {}) {
+    this.#config = config
   }
 
   /**
-   * Read value from the pre-loaded config. Make use of the `dot notation`
+   * Get all the config files as a tree of key-value pair
+   */
+  all() {
+    return this.#config
+  }
+
+  /**
+   * Read value from the config. Make use of the `dot notation`
    * syntax to read nested values.
    *
    * The `defaultValue` is returned when original value is `undefined`.
    *
-   * @example
-   * ```js
+   * ```ts
    * Config.get('database.mysql')
    * ```
    */
-  public get(key: string, defaultValue?: any): any {
-    return lodash.get(this.config, key, defaultValue)
+  get<T extends any>(key: string, defaultValue?: any): T {
+    return lodash.get(this.#config, key, defaultValue)
   }
 
   /**
-   * Fetch and merge an object to the existing config. This method is useful
-   * when you are fetching an object from the config and want to merge
-   * it with some default values.
-   *
-   * An optional customizer can be passed to customize the merge operation.
-   * The function is directly passed to [lodash.mergeWith](https://lodash.com/docs/4.17.10#mergeWith)
-   * method.
-   *
-   * @example
-   * ```js
-   * // Config inside the file will be merged with the given object
-   *
-   * Config.merge('database.mysql', {
-   *   host: '127.0.0.1',
-   *   port: 3306
-   * })
-   * ```
+   * Define the defaults for the a given key. If the value for the given
+   * key exists, then it will be merged with the defaults.
    */
-  public merge(key: string, defaultValues: object, customizer?: (...args: any[]) => any): any {
-    return lodash.mergeWith(defaultValues, this.get(key), customizer)
-  }
-
-  /**
-   * Defaults allows providers to define the default config for a
-   * module, which is merged with the user config
-   */
-  public defaults(key: string, value: any): void {
+  defaults(key: string, value: any): void {
     const existingValue = this.get(key)
-    if (existingValue) {
+    if (existingValue !== undefined) {
       lodash.mergeWith(value, existingValue)
     }
 
@@ -102,12 +79,11 @@ export class Config implements ConfigContract {
   /**
    * Update in memory value of the pre-loaded config
    *
-   * @example
-   * ```js
+   * ```ts
    * Config.set('database.host', '127.0.0.1')
    * ```
    */
-  public set(key: string, value: any): void {
-    lodash.set(this.config, key, value)
+  set(key: string, value: any): void {
+    lodash.set(this.#config, key, value)
   }
 }
